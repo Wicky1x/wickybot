ae22d368d48ebe39ffb361efc8f0078de591c38d
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-12 19:17:51 -0500

    inital comit

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'help',
+    description: "this is a help command!",
+    execute(message, args){
+       message.channel.send('Please Dm Wicky1X if there is a problem!');
+    }
+} 
