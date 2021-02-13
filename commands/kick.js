69211c697d9f22d27e71b5988576670f6b20dc64
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-13 12:22:52 -0500

    file

--- commands/kick.js
+++ commands/kick.js
@@ -1,20 +1,19 @@
 module.exports = {
     name: 'kick',
-    permissions: ["KICK_MEMBERS", "BAN_MEMBERS"],
-    description: "Kickt Mitglied!",
+    description: "Kicks a Member!",
     execute(client, message, args, Discord){
-  
+   
         const member = message.mentions.users.first();
         if(member){
             const memberTarger = message.guild.members.cache.get(member.id);
             memberTarger.kick();
-            message.channel.send("Mitglied wurde erfolgreich entfernt!");
-  
+            message.channel.send("Member got succesfully kicked!");
+   
         }else{
-            message.channel.send('Du kannst dieses Mitglied nicht entfernen!');
-  
-  
+            message.channel.send('You cant kick the member!');
+   
+   
         }
-  
+   
     }
-  }
+  } 
