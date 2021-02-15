632ec26efaf3f0331666f99952f93c17214a8094
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-15 17:43:54 -0500

    file

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,8 +1,9 @@
-const { Client } = require("discord.js");
-
 module.exsports = async (client) =>{
     const guild = client.guilds.cache.get('786628025054789702');
     setInterval(() =>{
         const memberCount = guild.memberCount;
-    });
+        const channel = guild.channel.cache.get('810630615753293904');
+        channel.setName(`Member Count ${memberCount.toLocaleString()}`);
+        console.log('Updating Member count');
+    }, 5000);
 }
