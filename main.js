bfe3c1e96e36bc677a1031a796cb4444ea97efb0
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-18 13:15:54 -0500

    file

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const memberCounter = require('./counters/member-counter');
 
-// //     client.user.setActivity(`Wicky1x on Youtube`, {type: 'WATCHING'}) // PLAYING, WATCHING,
-
-
 const fs = require ('fs');
 
 client.commands = new Discord.Collection();
@@ -14,31 +11,19 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-    client.user.setActivity(`Wicky1x on Youtube`, {type: 'WATCHING'}) // PLAYING, WATCHING, 
 
+client.once('ready', () =>{ 
+     console.log('Wicky Knight is online!');
+     memberCounter(client);
+     client.user.setActivity(`Wicky1x on Youtube`, {type: 'WATCHING'}) // PLAYING, WATCHING,  
+})
 
-
- client.on('guildMemberAdd', member =>{
-    
-      memberCounter(client);   
-    
-    
+client.on('guildMemberAdd', member =>{
+    memberCounter(client);   
     const channel = member.guild.channels.cache.find(channel => channel.name === "👋welcome");
-     if(!channel) return;
-
-     channel.send(`Welcome to Wicky's Place, ${member}, please read the rules in the rules channel.`)
- 
- 
- 
-    });
-
-
-
-    
-
-    
-
- 
+    if(!channel) return;
+    channel.send(`Welcome to Wicky's Place, ${member}, please read the rules in the rules channel.`)
+})
 
 
 client.login('ODA4MzQ5ODIxNjI1ODI3MzI4.YCFQbw.PyWqw2gO1g1E221QiF29B-EOtbc');
