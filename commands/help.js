4d79ac737886acbbcc8d969a1d6bf3b08d910c81
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-22 15:53:40 -0500

    file

--- commands/help.js
+++ commands/help.js
@@ -3,17 +3,17 @@ module.exports = {
     description: "this is a help command!",
     execute(client, message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
-       .setColor('#03cffc')
+       .setColor('#0cf2eb')
        .setTitle('Help')
        .setURL('')
-       .setDescription('Please read this to more understand our bot command.')
+       .setDescription('My prefix on this server is -')
        .addFields(
            {name: '-PP', value: 'This command will give you and acreate size of you PP.'},
            {name: '-Youtube', value: 'This command will show you my Youtube channel.'},
            {name: '-Twitch', value: 'This command will show you my Twitch channel.'},
            {name: '-Ping', value: 'This command will make the bot reply Pong!'},
            {name: '-Invite', value: 'This command will give you a link to invite my bot.'},
-           {name: '-play', value: 'This command will play whatever song you want!'},
+           {name: '-Play', value: 'This command will play whatever song you want!'},
            )
         .setFooter('Hope this helped you.');
        
