0426091f2534092cd0dd486c8543e5a0723a89dd
Wicky1x <78822290+Wicky1x@users.noreply.github.com>
2021-02-23 20:16:51 -0500

    file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const memberCounter = require('./counters/member-counter');
 
 const fs = require ('fs');
